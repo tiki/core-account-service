434034ff341a5f1a9d43c7c5fb7295f8949261a7
Mike Audi <mike@mytiki.com>
2023-01-18 15:05:02 -0600

    update tables
    #26

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -7,30 +7,38 @@
 -- ONE-TIME PASSWORD
 -- -----------------------------------------------------------------------
 CREATE TABLE IF NOT EXISTS otp(
-    otp_hashed TEXT NOT NULL,
+    otp_hashed TEXT PRIMARY KEY,
     issued_utc TIMESTAMP WITH TIME ZONE NOT NULL,
     expires_utc TIMESTAMP WITH TIME ZONE NOT NULL,
-    email TEXT,
-    PRIMARY KEY(otp_hashed)
+    email TEXT
 );
 
 -- -----------------------------------------------------------------------
 -- REFRESH TOKEN
 -- -----------------------------------------------------------------------
 CREATE TABLE IF NOT EXISTS refresh(
-    jti UUID NOT NULL,
+    jti UUID PRIMARY KEY,
     issued_utc TIMESTAMP WITH TIME ZONE NOT NULL,
-    expires_utc TIMESTAMP WITH TIME ZONE NOT NULL,
-    PRIMARY KEY(jti)
+    expires_utc TIMESTAMP WITH TIME ZONE NOT NULL
 );
 
 -- -----------------------------------------------------------------------
 -- USER INFO
 -- -----------------------------------------------------------------------
 CREATE TABLE IF NOT EXISTS user_info(
-    uid UUID NOT NULL,
+    uid UUID PRIMARY KEY,
     email TEXT NOT NULL UNIQUE,
     created_utc TIMESTAMP WITH TIME ZONE NOT NULL,
-    modified_utc TIMESTAMP WITH TIME ZONE NOT NULL,
-    PRIMARY KEY(uid)
+    modified_utc TIMESTAMP WITH TIME ZONE NOT NULL
+);
+
+-- -----------------------------------------------------------------------
+-- API ID
+-- -----------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS api_id(
+     api_id UUID PRIMARY KEY,
+     user_id TEXT REFERENCES user_info(uid) NOT NULL,
+     is_valid BOOLEAN NOT NULL DEFAULT FALSE,
+     created_utc TIMESTAMP WITH TIME ZONE NOT NULL,
+     modified_utc TIMESTAMP WITH TIME ZONE NOT NULL,
 );
