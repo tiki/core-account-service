986eff3cdf30cfd67240920b087c82d82bd6a76a
Mike Audi <mike@mytiki.com>
2023-11-08 15:53:17 -0600

    fix: add gh token exchange
    resolves #4

---
+++ src/main/java/com/mytiki/account/features/latest/exchange/github/GithubAOUser.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) TIKI Inc.
+ * MIT license. See LICENSE file in root directory.
+ */
+
+package com.mytiki.account.features.latest.exchange.github;
+
+public class GithubAOUser {
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
