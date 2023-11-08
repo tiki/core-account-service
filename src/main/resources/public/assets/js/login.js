122db33e4dac0186c39576a1ff04742d97f91b5e
Miro Benicio <mirodantas27@gmail.com>
2023-11-08 11:09:49 -0300

    feature: Client side otp login

---
+++ src/main/resources/public/assets/js/login.js
@@ -0,0 +1,25 @@
+document.getElementById("tikiLoginForm").addEventListener("submit", (event) => {
+  event.preventDefault();
+  const data = new FormData(event.target);
+  const dataObject = Object.fromEntries(data.entries());
+
+  let headers = new Headers();
+  headers.append("content-type", "application/json");
+  headers.append("accept", "application/json");
+  const options = {
+    method: "POST",
+    headers: headers,
+    body: JSON.stringify({ email: dataObject.email, notAnonymous: true }),
+  };
+
+  fetch("https://account.mytiki.com/api/latest/auth/otp", options)
+    .then(async (response) => {
+      const data = await response.json();
+      const self = window;
+      document.cookie = `TikiAccountOtpUser=${data.deviceId}; expires=${data.expires};`;
+      self.location.replace("./otp.html");
+    })
+    .catch((error) => {
+      console.log("error: ", error);
+    });
+});
