d06e2514f78b91ea54bf48cb01284e1c2b291509
Mike Audi <mike@mytiki.com>
2023-11-08 14:14:34 -0600

    fix: add page resolver for otp

--- src/main/java/com/mytiki/account/utilities/PublicResolver.java
+++ src/main/java/com/mytiki/account/utilities/PublicResolver.java
@@ -15,9 +15,11 @@ public class PublicResolver implements WebMvcConfigurer {
     public static String PAGES = "/pages";
     public static String ASSETS = "/assets";
     public static String PAGE_LOGIN = PAGES + "/login";
+    public static String PAGE_OTP = PAGES + "/otp";
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController(PAGE_LOGIN).setViewName(PAGE_LOGIN + PAGE_SUFFIX);
+        registry.addViewController(PAGE_OTP).setViewName(PAGE_OTP + PAGE_SUFFIX);
     }
 }
