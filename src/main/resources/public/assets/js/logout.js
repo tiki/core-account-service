2bc775bc5b4ca9eac10ea0ecbed89af307e52b44
Miro Benicio <mirodantas27@gmail.com>
2023-11-13 20:23:41 -0300

    feature: create logout page and script

--- src/main/resources/public/assets/js/logout.js
+++ src/main/resources/public/assets/js/logout.js
@@ -1,14 +1,12 @@
 window.onload = function () {
   const headers = new Headers();
-  headers.append("content-type", "application/json");
-  headers.append("accept", "application/json");
+  headers.append("Content-Type", "application/x-www-form-urlencoded");
   const options = {
     method: "POST",
     headers,
   };
   fetch("https://account.mytiki.com/api/latest/auth/revoke", options)
     .then(async (response) => {
-        console.log(response)
         window.location.href = `https://tiki-dev.mytiki.com`;
     })
     .catch((error) => {
