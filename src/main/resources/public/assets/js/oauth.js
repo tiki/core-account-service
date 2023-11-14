c1a48fba67cc476eb459f75ef3c2cee93cf6c829
Miro Benicio <mirodantas27@gmail.com>
2023-11-14 10:54:34 -0300

    fix:google server side oauth with token exchange

--- src/main/resources/public/assets/js/oauth.js
+++ src/main/resources/public/assets/js/oauth.js
@@ -2,51 +2,24 @@ const googleCliendId = "240428403253-buvkqgjamee7vqv9dmll0da69m1mpu04.apps.googl
 const githubClientId = "ebbf90361bcb8c527416"
 
 function githubLogin() {
+  localStorage.setItem("oauthType", "github")
   window.location.href = "https://github.com/login/oauth/authorize?scope=user:email&client_id=" + githubClientId
 }
 
 function googleLogin(){
-  window.location.href = "https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?"
+  localStorage.setItem("oauthType", "google")
+   window.location.href = "https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?"
     + "access_type=offline&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email"
     + "&prompt=select_account&redirect_uri=https://account.mytiki.com/pages/login&response_type=code&client_id=" + googleCliendId
 }
 
-function handleGoogleSignin(response) {
-  let headers = new Headers();
-  headers.append("Content-Type", "application/x-www-form-urlencoded");
-  headers.append("Accept", "application/json");
+function handleSignIn() {
+  const oauthType = localStorage.getItem("oauthType")
 
-  const options = {
-    method: "POST",
-    headers: headers,
-    body: new URLSearchParams({
-      grant_type: "urn:ietf:params:oauth:grant-type:token-exchange",
-      subject_token: response.credential,
-      subject_token_type: "urn:mytiki:params:oauth:token-type:google",
-      client_id: googleCliendId,
-      scope: "account:admin"
-    }),
-  };
-
-  fetch(`https://account.mytiki.com/api/latest/auth/token`, options)
-    .then((response) => response.json())
-    .then((response) => {
-      if (!response.readme_token) {
-        let element = document.getElementById("error");
-        element.innerHTML =
-          "Hey! Something got bad with your signin, try again in a few minutes";
-        element.classList.remove("hidden");
-        return;
-      }
-      window.location.href = `https://tiki-dev.mytiki.com/?auth_token=${response.readme_token}`;
-    })
-    .catch((err) => console.error(err));
-}
-
-function handleGithubSign() {
   const url_string = window.location.href.toLowerCase();
   const url = new URL(url_string);
   const code = url.searchParams.get("code");
+
   if (code) {
     const headers = new Headers();
     headers.append("Content-Type", "application/x-www-form-urlencoded");
@@ -58,8 +31,8 @@ function handleGithubSign() {
       body: new URLSearchParams({
         grant_type: "urn:ietf:params:oauth:grant-type:token-exchange",
         subject_token: code,
-        subject_token_type: "urn:mytiki:params:oauth:token-type:github",
-        client_id: githubClientId,
+        subject_token_type: "urn:mytiki:params:oauth:token-type:" + oauthType,
+        client_id: oauthType === "github" ? githubClientId : googleCliendId,
         scope: "account admin"
       }),
     };
@@ -81,11 +54,10 @@ function handleGithubSign() {
 }
 
 window.onload = function () {
-  handleGithubSign();
   google.accounts.id.initialize({
     client_id: googleCliendId,
-    callback: handleGoogleSignin,
   });
+  handleSignIn();
   document.getElementById('google-sign-in-btn').addEventListener('click', (e) => googleLogin())
   document.getElementById('github-sign-in-btn').addEventListener('click', (e) => githubLogin())
 };
