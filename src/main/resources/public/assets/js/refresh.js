1d98f20dd051bc58de9e10d8d9a274602835e649
Mike Audi <mike@mytiki.com>
2023-12-06 21:44:44 -0600

    fix: update js urls

--- src/main/resources/public/assets/js/refresh.js
+++ src/main/resources/public/assets/js/refresh.js
@@ -7,7 +7,7 @@ const headers = new Headers();
 headers.append("accept", "application/json");
 headers.append(
   "Content-Type",
-  "application/x-www-form-urlencoded;charset=UTF-8"
+  "application/x-www-form-urlencoded;charset=UTF-8",
 );
 
 const options = {
@@ -25,7 +25,7 @@ fetch("https://account.mytiki.com/api/latest/auth/token", options).then(
     document.getElementById("loading-container").classList.remove("hidden");
     const data = await response.json();
     if (data.readme_token) {
-      window.location.href = `https://tiki-dev.mytiki.com/?auth_token=${data.readme_token}`;
+      window.location.href = `https://mytiki.com/?auth_token=${data.readme_token}`;
     } else document.getElementById("loading-container").classList.add("hidden");
-  }
+  },
 );
